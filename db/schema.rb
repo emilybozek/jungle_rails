73ace64a69893e91d168b714895570305cf12c4e
Emily Bozek <emily.bozek@gmail.com>
2022-05-10 22:25:36 -0700

    create users helpers

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
