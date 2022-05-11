8a262a8a0b3d73a44334ed9016fab2750bf54963
Emily Bozek <emily.bozek@gmail.com>
2022-05-11 12:48:27 -0700

    fix login/logout styling by altering class-type on view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+      # Log the user in and redirect to the user's show page
     if user= User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       redirect_to '/login'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,11 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
 
+  # login/logout
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  get 'logout'  => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
