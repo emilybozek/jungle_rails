bf2e893a0753977f35355f56762c29913e3503e5
Emily Bozek <emily.bozek@gmail.com>
2022-05-11 21:37:03 -0700

    fix admin-auth bytesize bug

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"],
+  if: -> { ENV["ADMIN_PASSWORD"].present? }
 
   def show
     @product_amount = Product.count
